3623daecac1c0a7b6c3d1f91069dd2c9c400e326
Alexander Schaefer <alexander.schaefer@project-a.com>
2018-05-02 11:07:21 +0200

    moved to newer sdk version and added wheel dependency

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='facebook-ads-performance-downloader',
-    version='1.4.2',
+    version='1.4.3',
 
     description=("Downloads data from the Facebook Ads API to local files"),
 
     install_requires=[
-        'facebookads==2.11.1',
-        'click>=6.0'
+        'facebookads==2.11.3',
+        'click>=6.0',
+        'wheel>=0.29'
     ],
 
     packages=find_packages(),
