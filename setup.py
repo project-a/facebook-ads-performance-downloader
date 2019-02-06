9e64f5d9ba5b0ab4ed75a9d3bc5f59685d04cd84
Alexandru Pisarenco <alexandru.pisarenco@kfzteile24.de>
2019-02-06 11:06:32 +0100

    Bump version to 2.1.0 and update changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='facebook-ads-performance-downloader',
-    version='2.0.2',
+    version='2.1.0',
 
     description=("Downloads data from the Facebook Ads API to local files"),
 
